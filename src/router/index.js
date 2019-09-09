84f6622b266377fd9bc54b0743ff358a6c84799f
ivyzichou <947196171@qq.com>
2019-09-09 22:36:26 +0800

    feat:新建路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+// import HelloWorld from '@/components/HelloWorld'
 import home from '@/views/home'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    },
+    // {
+    //   path: '/',
+    //   name: 'HelloWorld',
+    //   component: HelloWorld
+    // },
     {
       path: '/home',
       name: 'home',
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -6,7 +6,7 @@
 
 <script>
 export default {
-  name: 'componentsTransWays'
+  name: 'home'
 }
 </script>
 
