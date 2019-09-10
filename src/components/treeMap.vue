91d5a4fea9aadbb11c58a1e5f223927b12dcfcc8
ivyzichou <947196171@qq.com>
2019-09-10 10:23:17 +0800

    feat:树图v1

--- src/components/treeMap.vue
+++ src/components/treeMap.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <ve-tree></ve-tree>
+    <ve-tree
+            :data = "chartData"></ve-tree>
   </div>
 </template>
 
@@ -10,6 +11,50 @@ export default {
   name: 'treeMap',
   components: {
     VeTree
+  },
+  props: {
+    columns: {
+      type: Array,
+      default: () => ['name', 'value']
+    },
+    rows: {
+      type: Array,
+      default: () => [{
+        name: 'tree',
+        value: [{
+          name: 'f',
+          value: 1,
+          children: [
+            {
+              name: 'a',
+              value: 1,
+              children: [
+                {
+                  name: 'a-a',
+                  value: 2
+                },
+                {
+                  name: 'a-b',
+                  value: 2
+                }
+              ]
+            }
+          ]
+        }]
+      }]
+    }
+  },
+  computed: {
+    chartData () {
+      return {
+        columns: this.columns,
+        rows: this.rows
+      }
+    }
+  },
+  data () {
+    return {
+    }
   }
 }
 </script>
